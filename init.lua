b763611f999eaf3ba91e25a1fc119d09aba2750d
Mark Rozumovych <fordmarkijan@icloud.com>
2025-05-13 15:10:36 +0200

    New theme and a few syntax-related plugins
    	•	Replaced OneDark Warmer with Catppuccin Macchiato
    	•	Added HiPhish/rainbow-delimiters.nvim
    	•	Added norcalli/nvim-colorizer.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- >>> Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -39,280 +39,342 @@ vim.g.mapleader = " "
 -- >>> Plugins
 require("lazy").setup({
 
-    -- 🌙 Тема: OneDark (Warmer)
-    {
-        "navarasu/onedark.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("onedark").setup({
-                style = "warmer", -- або 'dark', 'warmer', 'cool', 'deep', 'light'
-            })
-            require("onedark").load()
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				integrations = {
+					treesitter = true,
+					native_lsp = {
+						enabled = true,
+						virtual_text = {
+							errors = { "italic" },
+							hints = { "italic" },
+							warnings = { "italic" },
+							information = { "italic" },
+						},
+						underlines = {
+							errors = { "underline" },
+							hints = { "underline" },
+							warnings = { "underline" },
+							information = { "underline" },
+						},
+					},
+					lsp_trouble = true,
+					lsp_saga = false,
+					cmp = true,
+					gitsigns = true,
+					telescope = true,
+					which_key = true,
+				},
+				highlight_overrides = {
+					all = function(colors)
+						return {
+							["@keyword.import"] = { style = { "italic" }, fg = colors.mauve },
+							["@variable.builtin"] = { style = { "italic" }, fg = colors.red },
+							["@function.builtin"] = { fg = colors.blue },
+							["@comment"] = { fg = colors.overlay1, style = { "italic" } },
+						}
+					end,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 
-    -- 🧱 Lualine — статусбар
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "onedark",
-                    globalstatus = true, -- Єдиний статусбар на весь екран
-                },
-                sections = {
-                    lualine_a = { "mode" }, -- Показує поточний режим (NORMAL/INSERT)
-                    lualine_b = { "branch" }, -- Ім’я гілки Git
-                    lualine_c = {},
-                    lualine_x = { "fileformat", "filetype" }, -- Кодування, формат файлу, тип
-                    lualine_y = { "progress" }, -- Прогрес (рядок/стовпець)
-                    lualine_z = { "location" }, -- Позиція в файлі (рядок:стовпець)
-                },
-                inactive_sections = {
-                    lualine_a = { "filename" }, -- Назва файлу для неактивних буферів
-                    lualine_b = { "branch" }, -- Гілка Git
-                    lualine_c = {},
-                    lualine_x = { "location" }, -- Місце в файлі для неактивних
-                    lualine_y = {},
-                    lualine_z = {},
-                },
-                extensions = { "fugitive" }, -- Для Git, якщо потрібно
-            })
-        end,
-    },
+	-- 🧱 Lualine — статусбар
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+					globalstatus = true, -- Єдиний статусбар на весь екран
+				},
+				sections = {
+					lualine_a = { "mode" }, -- Показує поточний режим (NORMAL/INSERT)
+					lualine_b = { "branch" }, -- Ім’я гілки Git
+					lualine_c = {},
+					lualine_x = { "fileformat", "filetype" }, -- Кодування, формат файлу, тип
+					lualine_y = { "progress" }, -- Прогрес (рядок/стовпець)
+					lualine_z = { "location" }, -- Позиція в файлі (рядок:стовпець)
+				},
+				inactive_sections = {
+					lualine_a = { "filename" }, -- Назва файлу для неактивних буферів
+					lualine_b = { "branch" }, -- Гілка Git
+					lualine_c = {},
+					lualine_x = { "location" }, -- Місце в файлі для неактивних
+					lualine_y = {},
+					lualine_z = {},
+				},
+				extensions = { "fugitive" }, -- Для Git, якщо потрібно
+			})
+		end,
+	},
 
-    -- 🌲 Nvim-tree — файловий менеджер
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup()
-            vim.keymap.set("n", "<F2>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-        end,
-    },
+	-- 🌲 Nvim-tree — файловий менеджер
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup()
+			vim.keymap.set("n", "<F2>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+		end,
+	},
 
-    -- 🔍 Telescope — пошук
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.4",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        end,
-    },
+	-- 🔍 Telescope — пошук
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		end,
+	},
 
-    -- 🔧 Gitsigns — підсвічування змін
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
-        end,
-    },
+	-- 🔧 Gitsigns — підсвічування змін
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 
-    -- 🌲 Nvim-treesitter — синтаксичний аналізатор
-    {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = "all", -- Встановити парсери для всіх підтримуваних мов
-                highlight = {
-                    enable = true, -- Включити підсвічування синтаксису
-                    additional_vim_regex_highlighting = false, -- вимкнення додаткового підсвітлення
-                },
-                incremental_selection = {
-                    enable = true, -- Включити інкрементальний вибір
-                },
-            })
-        end,
-    },
+	-- 🌲 Nvim-treesitter — синтаксичний аналізатор
+	{
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all", -- Встановити парсери для всіх підтримуваних мов
+				highlight = {
+					enable = true, -- Включити підсвічування синтаксису
+					additional_vim_regex_highlighting = false, -- вимкнення додаткового підсвітлення
+				},
+				incremental_selection = {
+					enable = true, -- Включити інкрементальний вибір
+				},
+			})
+		end,
+	},
 
-    -- Пошуковий інструмент fd (sharkdp/fd)
-    {
-        "sharkdp/fd",
-        run = ":UpdateFd",
-    },
+	-- Пошуковий інструмент fd (sharkdp/fd)
+	{
+		"sharkdp/fd",
+		run = ":UpdateFd",
+	},
 
-    -- ripgrep
-    {
-        "BurntSushi/ripgrep",
-        run = ":UpdateRipgrep",
-    },
+	-- ripgrep
+	{
+		"BurntSushi/ripgrep",
+		run = ":UpdateRipgrep",
+	},
 
-    -- LSP менеджер
-    {
-        "williamboman/mason.nvim",
-        build = ":MasonUpdate",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	-- LSP менеджер
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    -- Інтеграція Mason з LSP
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "pyright", "lua_ls", "bashls", "jsonls", "html", "cssls" },
-            })
-        end,
-    },
+	-- Інтеграція Mason з LSP
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "pyright", "lua_ls", "bashls", "jsonls", "html", "cssls" },
+			})
+		end,
+	},
 
-    -- Основна LSP підтримка
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+	-- Основна LSP підтримка
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-            -- Lua для конфігів
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                        workspace = { checkThirdParty = false },
-                    },
-                },
-            })
-        end,
-    },
+			-- Lua для конфігів
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
+					},
+				},
+			})
+		end,
+	},
 
-    -- Автодоповнення
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "L3MON4D3/LuaSnip", -- snippets
-            "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
+	-- Автодоповнення
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip", -- snippets
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
-            })
-        end,
-    },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
 
-    -- 📐 Автоформатер: conform.nvim
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            formatters_by_ft = {
-                python = { "black" },
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                json = { "prettier" },
-                html = { "prettier" },
-                css = { "prettier" },
-                sh = { "shfmt" },
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
-        },
-    },
+	-- 📐 Автоформатер: conform.nvim
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				python = { "black" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				json = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				sh = { "shfmt" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		},
+	},
 
-    -- 🧪 Лінтер: nvim-lint + ruff
-    {
-        "mfussenegger/nvim-lint",
-        config = function()
-            require("lint").linters_by_ft = {
-                python = { "ruff" },
-            }
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-                callback = function()
-                    require("lint").try_lint()
-                end,
-            })
-        end,
-    },
+	-- 🧪 Лінтер: nvim-lint + ruff
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			require("lint").linters_by_ft = {
+				python = { "ruff" },
+			}
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
 
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
-        keys = {
-            { "<leader>xx", "<cmd>Trouble<cr>", desc = "Trouble" },
-            { "<leader>xq", "<cmd>Trouble quickfix<cr>", desc = "QuickFix" },
-            { "<leader>xd", "<cmd>Trouble diagnostics<cr>", desc = "Document Diagnostics" },
-        },
-    },
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+		keys = {
+			{ "<leader>xx", "<cmd>Trouble<cr>", desc = "Trouble" },
+			{ "<leader>xq", "<cmd>Trouble quickfix<cr>", desc = "QuickFix" },
+			{ "<leader>xd", "<cmd>Trouble diagnostics<cr>", desc = "Document Diagnostics" },
+		},
+	},
 
-    {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim", -- Плагін lsp_lines
-        config = function()
-            -- Ініціалізація плагіна
-            require("lsp_lines").setup()
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim", -- Плагін lsp_lines
+		config = function()
+			-- Ініціалізація плагіна
+			require("lsp_lines").setup()
 
-            -- Вимкнути стандартний virtual_text для діагностик
-            vim.diagnostic.config({
-                virtual_text = false,
-                update_in_insert = true,
-            })
+			-- Вимкнути стандартний virtual_text для діагностик
+			vim.diagnostic.config({
+				virtual_text = false,
+				update_in_insert = true,
+			})
 
-            -- Можна додати гарячі клавіші для перемикання:
-            vim.keymap.set("n", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-        end,
-    },
+			-- Можна додати гарячі клавіші для перемикання:
+			vim.keymap.set("n", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+		end,
+	},
 
-    -- 📦 Lspsaga — покращене LSP UI
-    {
-        "nvimdev/lspsaga.nvim",
-        event = "LspAttach",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lspsaga").setup({})
-        end,
-    },
+	-- 📦 Lspsaga — покращене LSP UI
+	{
+		"nvimdev/lspsaga.nvim",
+		event = "LspAttach",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lspsaga").setup({})
+		end,
+	},
 
-    -- 🧩 Автодоповнення для парних символів
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup()
-        end,
-    },
+	-- 🧩 Автодоповнення для парних символів
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-    }
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			local rainbow_delimiters = require("rainbow-delimiters")
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 })
 
 vim.opt.signcolumn = "yes:1"
 
 -- >>> Keymap: <leader>f — ручне форматування
 vim.keymap.set("n", "<leader>f", function()
-    require("conform").format({ async = true, lsp_fallback = true })
+	require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Форматувати файл" })
 
 local map = vim.keymap.set
@@ -322,6 +384,8 @@ map("n", "<F2>", ":NvimTreeToggle<CR>", { desc = "Файловий менедж�
 map("n", "<leader>q", ":bd<CR>", { desc = "Закрити буфер" })
 map("n", "<leader>w", ":w<CR>", { desc = "Зберегти" })
 map("n", "<leader>x", ":q<CR>", { desc = "Вийти" })
+-- Вийти з режиму термінала на Esc
+map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
 
 -- Telescope
 local builtin = require("telescope.builtin")
@@ -340,5 +404,5 @@ map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Наступн
 
 -- Форматування
 map("n", "<leader>f", function()
-    require("conform").format({ async = true, lsp_fallback = true })
+	require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Форматувати файл" })
